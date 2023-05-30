e26f19bc8a6d07994aa4a691524ccc4927039e4e
Abiodun Shittu <jerrywizklay1@gmail.com>
2023-05-30 23:54:23 +0100

    Error handler: Unauthorized

--- 0x01-Basic_authentication/api/v1/views/index.py
+++ 0x01-Basic_authentication/api/v1/views/index.py
@@ -26,7 +26,7 @@ def stats() -> str:
     return jsonify(stats)
 
 @app_views.route('/unauthorized/', strict_slashes=False)
-def unauthorized() -> None:
+def unauthorized() -> str:
   """ GET /api/v1/unauthorized
   Return:
     - Unauthorized
