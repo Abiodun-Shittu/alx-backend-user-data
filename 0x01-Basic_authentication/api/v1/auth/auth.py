fdec1973f2328cb616fbafe2727647123adfcdee
Abiodun Shittu <jerrywizklay1@gmail.com>
2023-05-31 00:04:10 +0100

    Auth class

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -4,6 +4,7 @@
 from flask import request
 from typing import List, TypeVar
 
+
 class Auth:
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
         """ Check if authentication is required for the given path
